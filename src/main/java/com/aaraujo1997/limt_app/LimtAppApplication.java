4b0437683976db39f89b33ae7d3c24fe9b999808
aaraujo97 <91360458+aaraujo1997@users.noreply.github.com>
2022-05-25 19:18:40 -0400

    Uploaded bootstrapped initialzr files

---
+++ src/main/java/com/aaraujo1997/limt_app/LimtAppApplication.java
@@ -0,0 +1,13 @@
+package com.aaraujo1997.limt_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimtAppApplication.class, args);
+	}
+
+}
